6a86b59d867a72c7f2f231fc923e83edc032cd53
ignaciosm <ignaciosm@gmail.com>
2019-10-14 15:04:09 -0500

    mvp

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Route } from 'react-router-dom';
 import './App.css';
 import { Home, About, Contact, Navigation } from './components';
 
-const App = () => (
+const App = (props) => (
   <div>
-    <Navigation />
+    <Navigation>
+      <Route exact path="/" component={Home} />
+      <Route exact path="/about" component={About} />
+      <Route exact path="/contact" component={Contact} />
+    </Navigation>
   </div>
 );
 
